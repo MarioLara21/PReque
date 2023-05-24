ab6eb05791ebe565cda3f4170c45d8874dd7f41b
MarioLara21 <mariolara.21@estudianTEC.cr>
2023-05-24 15:44:31 -0600

    Pantalla asignación reservas

---
+++ app/src/main/java/com/example/proyecto1/Gestion_Asignacion.java
@@ -0,0 +1,14 @@
+package com.example.proyecto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gestion_Asignacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_asignacion);
+    }
+}
