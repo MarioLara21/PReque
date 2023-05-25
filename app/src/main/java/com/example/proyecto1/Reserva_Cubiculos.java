3da227940111ea36005a2728c4196de74fb37e32
MarioLara21 <mariolara.21@estudianTEC.cr>
2023-05-24 18:24:34 -0600

    Pantalla reserva cubículos

---
+++ app/src/main/java/com/example/proyecto1/Reserva_Cubiculos.java
@@ -0,0 +1,14 @@
+package com.example.proyecto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reserva_Cubiculos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserva_cubiculos);
+    }
+}
