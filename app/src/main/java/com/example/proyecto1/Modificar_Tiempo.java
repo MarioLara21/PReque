d665c34b5e0845889b65f7eb097e7a07c4c544a9
MarioLara21 <mariolara.21@estudianTEC.cr>
2023-05-25 10:09:36 -0600

    Pantalla modificar tiempo

---
+++ app/src/main/java/com/example/proyecto1/Modificar_Tiempo.java
@@ -0,0 +1,14 @@
+package com.example.proyecto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Modificar_Tiempo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_tiempo);
+    }
+}
